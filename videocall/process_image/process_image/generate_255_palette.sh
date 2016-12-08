5fa0351a202f8cc30b2be68df360dd211e929378
pjgat09 <pjgat09@gmail.com>
2016-12-08 11:05:20 -0500

    Videocall: Disable the alpha channel output on the palette.

--- videocall/process_image/process_image/generate_255_palette.sh
+++ videocall/process_image/process_image/generate_255_palette.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-convert $1 -colors 255 -unique-colors -depth 8 txt:- > palette.txt
+convert $1 -colors 255 -unique-colors -depth 8 -alpha off txt:- > palette.txt
 perl process_255_palette.pl > palette.bin
 rm palette.txt
