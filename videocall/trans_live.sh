127409d407981696f915f61aa592a91d5473fe55
pjgat09 <pjgat09@gmail.com>
2017-01-05 11:08:46 -0500

    Videocall: Fixed the live bash script

--- videocall/trans_live.sh
+++ videocall/trans_live.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -O2 process_image_stream/process_image/transout_full_live.cpp process_image_stream/process_image/octree.cpp process_image_stream/process_image/trans.cpp -o process_image_stream/process_image/transout_full
+g++ -O2 process_image_stream/process_image/transout_full_live.cpp process_image_stream/process_image/octree.cpp process_image_stream/process_image/trans.cpp -o process_image_stream/process_image/transout_full_live
