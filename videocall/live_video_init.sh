df61a5e33b8bbdabae85b2a950ba19a0b27c1ed2
pjgat09 <pjgat09@gmail.com>
2017-01-08 23:50:16 -0500

    Videocall: Fixed the video init bash script

--- videocall/live_video_init.sh
+++ videocall/live_video_init.sh
@@ -6,4 +6,4 @@ V_DEVICE=/dev/ttyACM2
 
 (./splitaudio2 /dev/stdout mm /run/user/1001/timing.tmp < /tmp/aaaL | ./pipebuf 10000 | python3 ./psoc/Scripts/play_r16y.py $L_CHANNEL_DEVICE /dev/stdin) &
 (./splitaudio2 /dev/stdout mm /run/user/1001/timing2.tmp < /tmp/aaaR | ./pipebuf 10000 | python3 ./psoc/Scripts/play_r16y.py $R_CHANNEL_DEVICE /dev/stdin) &
-(./process_image_stream/process_image/transout_full 5 < /tmp/vvv | python3 ./psoc/Scripts/play_r16y_cmd_resync_stdin.py $V_CHANNEL_DEVICE)
+(./process_image_stream/process_image/transout_full 5 < /tmp/vvv | python3 ./psoc/Scripts/play_r16y_cmd_resync_stdin.py $V_DEVICE)
