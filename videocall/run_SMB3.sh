8fd3fd1613848faa03622f77ab886b61aea86a26
pjgat09 <pjgat09@gmail.com>
2017-01-11 00:55:15 -0500

    Videocall: Updated the SMB3 run script

--- videocall/run_SMB3.sh
+++ videocall/run_SMB3.sh
@@ -2,4 +2,5 @@
 
 source port_assignments.sh
 
-python3 ./psoc/Scripts/play_r16y_SMB3.py $L_CHANNEL_DEVICE ../games/SMB3_AGDQ/v1/smb3_170110.r16m
+#python3 ./psoc/Scripts/play_r16y_SMB3.py $L_CHANNEL_DEVICE ../games/SMB3_AGDQ/v1/smb3_170110.r16m
+python3 ./psoc/Scripts/play_r16y_SMB3.py $L_CHANNEL_DEVICE ../games/SMB3_AGDQ/v2/smb3_pcm.r16m
