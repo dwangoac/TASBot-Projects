419d0741412a2837cb17cff437bbb81e2ef7bc86
pjgat09 <pjgat09@gmail.com>
2017-01-13 00:59:00 -0500

    Videocall: Video init script now attempts to recover the R and L channels before continuing.

--- videocall/live_video_init.sh
+++ videocall/live_video_init.sh
@@ -23,6 +23,9 @@ fi;
 
 source port_assignments.sh
 
+python3 ./psoc/Scripts/recover.py $L_CHANNEL_DEVICE
+python3 ./psoc/Scripts/recover.py $R_CHANNEL_DEVICE
+
 (./splitaudio2 /dev/stdout mm /run/user/1001/timing.tmp < /tmp/aaaL | ./pipebuf 10000 | python3 ./psoc/Scripts/play_r16y.py $L_CHANNEL_DEVICE /dev/stdin) &
 (./splitaudio2 /dev/stdout mm /run/user/1001/timing2.tmp < /tmp/aaaR | ./pipebuf 10000 | python3 ./psoc/Scripts/play_r16y.py $R_CHANNEL_DEVICE /dev/stdin) &
 (./process_image_stream/process_image/transout_full 5 < /tmp/vvv | python3 ./psoc/Scripts/play_r16y_cmd_resync_stdin.py $V_DEVICE)
