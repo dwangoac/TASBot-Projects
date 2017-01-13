48b3d36f5982c431c05701a4dfc93624d809f5bd
pjgat09 <pjgat09@gmail.com>
2017-01-13 14:36:17 -0500

    Videocall: Adding scripts to run SSBM

---
+++ videocall/play_ssb1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source port_assignments.sh
+
+python3 ./psoc/Scripts/play_r16y_cmd_resync_stdin.py $V_DEVICE < ./supern64_anim/supern64_anim_fadeout.r16m
+python3 ./psoc/Scripts/play_r16y_cmd_resync_stdin.py $V_DEVICE < ./bid_war/ssb_anim.r16m
---
+++ videocall/play_ssb2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./live_video_run.sh ~/sm64-movie/SSBM-VBR1P-50Mbps-AAC-320kbps-44_1-Stereo.mp4
+./shutup.sh
