c2ce9f355927b9853fedcc84697b71deec083302
pjgat09 <pjgat09@gmail.com>
2017-01-14 00:47:56 -0500

    MM1: Adding v6 Videocall: Using that new movie

--- videocall/run_MM1.sh
+++ videocall/run_MM1.sh
@@ -4,4 +4,5 @@ source port_assignments.sh
 
 #python3 ./psoc/Scripts/play_r16y.py $R_CHANNEL_DEVICE ../games/MM1/pcm_v3/mm1_pcm_v3.r16m
 #python3 ./psoc/Scripts/play_r16y.py $R_CHANNEL_DEVICE ../games/MM1/pcm_v4/mm1_170110.r16m 
-python3 ./psoc/Scripts/play_r16y.py $R_CHANNEL_DEVICE ../games/MM1/v5/mm1.r16m 
+#python3 ./psoc/Scripts/play_r16y.py $R_CHANNEL_DEVICE ../games/MM1/v5/mm1.r16m 
+python3 ./psoc/Scripts/play_r16y.py $R_CHANNEL_DEVICE ../games/MM1/v6/mm1.r16m 
